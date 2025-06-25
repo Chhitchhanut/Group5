0ab8be8d1d0a8b4a035c7f5d886df2bc1ccb4d79
RINA.HO <RINA.HO@pnc.lan>
2025-06-25 20:01:35 +0700

    ri

--- mona.py
+++ mona.py
@@ -3,4 +3,11 @@ sum = 0
 for char in array:
     sum += char
 print(sum)
+#
 
+array = [1,2,3,4,0,5,11,6,7,8,9,10]
+sum_odd = 0
+for char in array:
+    if char % 2 == 1:
+        sum_odd += char
+print(sum_odd)
