07ad34e1bbd52708223fc226ef4d0b7711046416
RINA.HO <RINA.HO@pnc.lan>
2025-06-25 20:03:31 +0700

    momo

--- mona.py
+++ mona.py
@@ -11,3 +11,10 @@ for char in array:
     if char % 2 == 1:
         sum_odd += char
 print(sum_odd)
+#
+array = [1,2,3,4,0,5,11,6,7,8,9,10,]
+max = array[0]
+for i in range(len(array)):
+    if  int(array[i]) % 2 == 0 and array[i] > max:
+        max = array[i]
+print(max)
