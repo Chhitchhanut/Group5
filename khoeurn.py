ee4c869ef5c7b64b82ccc886e3cbf473f06b651a
sokkhoeurnleng <nakesokkhoeurn@gmail.com>
2025-06-26 09:35:55 +0700

    student

--- khoeurn.py
+++ khoeurn.py
@@ -11,3 +11,15 @@ for i in range(len(student_ages)):
     if student_ages[i]> max:
         max = student_ages[i]
 print(max)
+
+tudent_ages = [15, 18, 20, 17, 16, 25]
+num = 0
+for i in range(len(student_ages)):
+    num += student_ages[i]
+print(num/len(student_ages))
+
+student_ages = [15, 18, 20, 17, 16, 25]
+for i in range(len(student_ages)):
+    if student_ages[i]<18:
+        student_ages[i]=22
+print(student_ages)
