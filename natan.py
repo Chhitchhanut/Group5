57fb59dc3fbeaec80f89ba376e8c5afeabda800d
SATAN.BUNTIT <SATAN.BUNTIT@PNC.LAN>
2025-06-26 09:17:42 +0700

    add file

--- natan.py
+++ natan.py
@@ -1,3 +1,10 @@
 fruits = ['banana', 'coconut', 'manago', 'jackfruit']
 for i in fruits:
     print(i)
+
+    student_score = [90, 49, 68, 40, 92, 50, 45, 59]
+counter = 0
+for i in range(len(student_score)):
+    if student_score[i] > 50:
+        counter = counter + 1
+print(counter)
